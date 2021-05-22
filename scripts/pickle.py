3052c844147823ffa48256d13514a9dfaeeacf0f
Alper Demirel <36865822+alperdem1rel@users.noreply.github.com>
2021-05-23 01:26:31 +0300

    Update pickle.py

--- scripts/pickle.py
+++ scripts/pickle.py
@@ -1,11 +1,9 @@
 import pickle
 
-import pandas as pd
 import numpy as np
 from pathlib import Path
 
 from sklearn.linear_model import LinearRegression
-from sklearn.ensemble import GradientBoostingRegressor
 
 from sklearn.datasets import load_boston
 from sklearn.model_selection import train_test_split
